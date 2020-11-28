f6f4e9826f227c7017f9a470b403e8f3d69b5805
Prasad Bayi <74462843+prasadbayi@users.noreply.github.com>
2020-11-28 08:12:51 +0530

    initial commit.
    initial commit.

---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { freeApiService } from './services/freeapi.service';
+// import { Comments } from './classes/comments';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-app - bayi';
+  lstcomments: any;
+
+  constructor(private _freeApiService: freeApiService) {
+
+  }
+
+  // lstcomments:Comments[];
+  
+  ngOnInit() {
+
+    this._freeApiService.getcomments()
+    .subscribe
+    (
+      data=>
+      {
+          this.lstcomments = data;
+
+      }
+    )
+  }
+}
---
+++ src/app/services/freeapi.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+@Injectable()
+export class freeApiService
+{
+
+    constructor(private httpclient: HttpClient) {}
+    getcomments(): Observable<any> {
+        return this.httpclient.get("http://localhost:8082/catalog/products");
+
+    }
+}
